f1b6adcca6f2538f054f1a0dd85bdc3f26f1b6df
Thiago Martins <thiagomartins.silva@sou.unifal-mg.edu.br>
2024-10-10 14:00:53 -0300

    Começo da Implementação - Árvore Binária

--- arvores.c
+++ arvores.c
@@ -4,6 +4,56 @@
 #include <time.h>       
 #include "arvores.h"
 
+// Função para criar um novo nó na árvore binária.
+struct arvbin *Novo_no(int valor){
+    // Alocamos o espaço de uma posição da struct para armazenar o nó, definimos o valor do nó com o parâmetro passado e iniciamos suas subárvores com NULL.
+    struct arvbin *tmp = (struct arvbin*)malloc(sizeof(struct arvbin));
+    tmp->valor = valor;
+    tmp->esq = tmp->dir = 0;
+
+    return tmp;
+}
+
+// Função para buscar um nó na árvore binária.
+struct arvbin *Busca_arvbin(int valor){
+    struct arvbin *tmp = root;
+    // Caso root == NULL, a árvore está vazia.
+    if(root == NULL){
+        return root;
+    }
+
+    // Enquanto o valor desejado não for encontrado e o nó não for NULO, continua percorrendo a árvore.
+    while(tmp->valor != valor && tmp->valor != NULL){
+        if(valor == tmp->valor){
+            return tmp;
+        }else{
+            if(valor > tmp->valor){
+                tmp = tmp->dir;
+            }else if(valor < tmp->valor){
+                tmp = tmp->esq;
+            }
+        }
+    }
+
+    // Caso o valor não seja encontrado na árvore, a função retorna NULL.
+    return tmp;
+}
+
+// Função para inserir um nó na árvore binária.
+struct arvbin *Inserir_bin(int valor){
+    // Nó a ser inserido na árvore binária.
+    struct arvbin *no_inserir = Novo_no(valor);
+
+    // Posição em que o nó deve ser inserido.
+    struct arvbin *pos_inserir = Busca_arvbin(valor);
+    
+    if(pos_inserir == NULL){
+        pos_inserir = no_inserir;
+    }
+
+    return no_inserir;
+}
+
 double arvore_binaria(int instancia_num) {
     double tempo = 0;
     clock_t begin = clock();
@@ -41,6 +91,9 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
     
+    // Iniciando a raiz da árvore como NULL. (Árvore começa vazia).
+    root = NULL;
+
     double tempo_n_balanceada = arvore_binaria(instancia_num);
     double tempo_balanceada = arvore_balanceada(instancia_num);
 
--- arvores.h
+++ arvores.h
@@ -2,6 +2,10 @@
 #define _H_ARVORES
 
 // Implementar qualquer struct que for necessária aqui
+struct arvbin{
+    int valor;
+    struct arvbin *esq, *dir;
+}*root; // Ponteiro para armazenar a raiz da árvore.
 
 double arvore_binaria(int instancia_num);
 double arvore_balanceada(int instancia_num);
